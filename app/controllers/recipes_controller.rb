4e0171216e2f0a2a1e9d6579b44d6aeb1938efb7
rahul357 <rahul40800@gmail.com>
2016-07-20 18:48:17 +0530

    strong params and error messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,20 @@ class RecipesController < ApplicationController
   end
 
   def create
-    binding.pry
+   @recipe =Recipe.new(recipe_params)
+   @recipe.chef = Chef.find(2)
+
+   if @recipe.save
+     flash[:success] = "Your Recipe was Successfully saved!!!"
+       redirect_to recipes_path
+   else
+   	render :new
+   end
   end
+
+  private def recipe_params
+    params.require(:recipe).permit(:name,:summary,:description)
+  end
+
 
 end
